a8d3e6d2f0b9ac199056e4e14756c74bf8a302ba
Gael <gael1142@gmail.com>
2020-01-17 23:22:58 +0300

    - change pathes to DB for docker build

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/sbin/dinit /bin/sh
 
-java -Dspring.profiles.active=docker -jar /srv/learnphrasebot.jar
+java -Dspring.profiles.active=docker -Dspring.datasource.url=jdbc:h2:file:/srv/data/botdb -Dspring.liquibase.url=jdbc:h2:file:/srv/data/botdb -jar /srv/learnphrasebot.jar
